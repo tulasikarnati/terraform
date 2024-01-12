6975a446ed5b487dc4fa72c8b4edc3cbf89be9f4
Tulasi Karnati <karnatitulasi2099@gmail.com>
2024-01-12 00:10:56 +0000

    terraform

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,7 +5,15 @@ terraform {
       version = "5.31.0"
     }
   }
+
+  backend "s3" {
+    bucket = "daws94t-remote-state"
+    key    = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "daws94t-locking"
+  }
 }
+
 
 provider "aws" {
   region = "us-east-1"
